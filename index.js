7649752883c83afb987f57afa5a2f6166010ec00
João Adriano Freitas <jaafreitas@gmail.com>
2017-10-02 20:46:18 -0300

    Improving the server to allow crossdomain.xml and SBX project files locally.

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-var BBCMicrobit = require('bbc-microbit')
+var BBCMicrobit = require('bbc-microbit');
+const fs = require('fs');
+const path = require('path');
 
 var microbitConnected = false;
 
@@ -6,13 +8,26 @@ var BUTTON_VALUE_MAPPER = ['Not Pressed', 'Pressed', 'Long Press'];
 
 console.log('*** BBC micro:bit Scratch extension ***')
 
-var http = require('http'),
-    fs = require('fs'),
-    index = fs.readFileSync(__dirname + '/scratch_microbit.js');
+var http = require('http');
 
 var app = http.createServer(function(req, res) {
-    res.writeHead(200, {'Content-Type': 'application/javascript'});
-    res.end(index);
+  console.log('http: read ' + req.url);
+  const map = {
+    '.xml': 'application/xml',
+    '.js': 'application/javascript',
+    '.sbx': 'application/octet-stream'
+  };
+  const ext = path.parse(req.url).ext;
+
+  fs.readFile(__dirname + req.url, function(err, data) {
+    if (err) {
+      res.statusCode = 404;
+      res.end();
+    } else {
+      res.setHeader('Content-type', map[ext] || 'text/plain');
+      res.end(data);
+    }
+  });
 });
 
 var io = require('socket.io').listen(app.listen(3000));
